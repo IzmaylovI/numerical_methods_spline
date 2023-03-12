bbae130e29c4a24092bf0aa4afcac1f9ee6c2cbb
IzmaylovI <75639514+IzmaylovI@users.noreply.github.com>
2023-03-12 22:02:35 +0300

    first_version

---
+++ Project9/Function.h
@@ -0,0 +1,4 @@
+#pragma once
+
+double* Tridiagonal_matrix_algorithm(int n, double* A, double* B, double* C, double* D);
+void interp_spline(int grid_dim, double step, double* X, double* Y, double* a, double* b, double* c, double* d);
